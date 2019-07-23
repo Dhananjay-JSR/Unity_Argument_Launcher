63991e7550fb9dd3c85c7c77983b50111268867c
Dhananjay-JSR <47073516+Dhananjay-JSR@users.noreply.github.com>
2019-07-23 12:20:50 +0530

    New
    Test

---
+++ Unity/Messga_eBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Messga_eBox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string args = "zxzxz";
+        args = string.Join("\n", System.Environment.GetCommandLineArgs());
+        var target = gameObject.GetComponent<Text>();
+        target.text = args;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Visual Studio Solution/Argument/Argument/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Argument
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Argument Taken  -  " + textBox1.Text);
+            start_program();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void start_program()
+        {
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+            pProcess.StartInfo.FileName = @"C:\zzzzzzzzzzzzzzz\2_0_1_1.exe";                            // FILE LOCATION
+            pProcess.StartInfo.Arguments = textBox1.Text;                                                      //argument               
+            pProcess.StartInfo.UseShellExecute = false;
+            pProcess.StartInfo.RedirectStandardOutput = true;
+            pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            pProcess.StartInfo.CreateNoWindow = true; //not diplay a windows
+            pProcess.Start();
+            string output = pProcess.StandardOutput.ReadToEnd(); //The output result
+            pProcess.WaitForExit();
+        }
+    }
+}
